bbe043e3a562d565721fd5435501a9a966d94588
Federico B. Samoranos <fbsamoranos@unionbankph.com>
2022-11-03 19:42:18 +0800

    add client server

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require( 'express' )
 const bodyParser = require( 'body-parser' )
 const mysql = require( 'mysql' )
 const cors = require('cors')
+const path = require( 'path' )
 
 const app = express()
+
+app.use( express.static( path.join(__dirname + '/public' ) ) )
 const port = process.env.PORT || 5000
 
 app.use( bodyParser.urlencoded( { extended: false } ) )
